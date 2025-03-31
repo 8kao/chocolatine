c3af719435b7246569fa418c565e5b89ad8852f7
mathilde <mathilde@epitech.eu>
2025-03-31 16:22:18 +0200

    push

--- test.c
+++
@@ -1,5 +0,0 @@
-int main (  int ac, char **argv) {
-    if (!argv[1])
-        return 84;
-    int nb = 0;
-}
---
+++ test2.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2025
+** chocolatine
+** File description:
+** test2
+*/
+
+int main(void)
+{
+    int nb = 0;
+
+    return 0;
+}
